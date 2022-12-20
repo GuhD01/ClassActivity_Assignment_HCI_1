e03baa96af82810271ce255dc1e1a7ad8024093a
GuhD <88705355+GuhD01@users.noreply.github.com>
2022-12-20 21:39:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Please put your answer here
+$(function() {
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+  $('#close-modal').click(function(){
+    $('#signup-modal').fadeOut();
+  });
+});
